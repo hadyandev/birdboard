8a5383f97d20c31f4f2ff4e20a3b678bc3630eec
Hadyan Ahmad <hadyandev@gmail.com>
2020-01-14 16:34:29 +0700

    09. Go Go Gadget Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 module.exports = {
     theme: {
-        extend: {}
+        extend: {
+            colors: {
+                grey: "rgba(0, 0, 0.4)",
+                "grey-light": "#F5F6F9"
+                // "grey-dark": "rgba(0, 0, 0.4)"
+            }
+        }
     },
     variants: {},
-    plugins: [],
-    options: {
-        prefix: "tw-"
-    }
+    plugins: []
 };
