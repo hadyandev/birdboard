3a6ad528e204c734c8c711b556006b0f7dd287d4
Hadyan Ahmad <hadyandev@gmail.com>
2020-01-14 14:47:26 +0700

    08. Prepping the Frontend

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,33 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Create</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('content')
     <h1>Create a Project</h1>
 
     <form method="POST" action="/projects">
         @csrf
-        <div class="field">
-            <label for="" class="label">Title</label>
-            <div class="control">
-                <input type="text" name="title" class="input">
-            </div>
+        <div class="form-group">
+            <label for="">Title</label>
+            <input type="text" name="title" class="form-control">
         </div>
-        <div class="field">
-            <label for="" class="label">Description</label>
-            <div class="control">
-                <textarea name="description" id="" cols="30" rows="10"></textarea>
-            </div>
+        <div class="form-group">
+            <label for="">Description</label>
+            <textarea name="description" class="form-control" id="" cols="30" rows="10"></textarea>
         </div>
-        <div class="field">
-            <div class="control">
-                <button type="submit">Create Project</button>
-            </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-dark">Create Project</button>
+            <a href="/projects" class="btn btn-warning">Cancel</a>
         </div>
     </form>
-</body>
-</html>
+@endsection
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+        extend: {}
+    },
+    variants: {},
+    plugins: [],
+    options: {
+        prefix: "tw-"
+    }
+};
